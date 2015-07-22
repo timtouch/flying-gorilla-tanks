ef2b21a07ab354e5d1295520d12c2881173e6529
Timothy Touch <chaosbounder@gmail.com>
2015-07-22 21:38:52 +0000

    Added more features to makePoll.html/.controller.js

--- client/app/makepoll/makepoll.controller.js
+++ client/app/makepoll/makepoll.controller.js
@@ -1,15 +1,38 @@
 'use strict';
 
 angular.module('workspaceApp')
-  .controller('MakepollCtrl', function ($scope) {
+  .controller('MakepollCtrl', function($scope){
     $scope.message = 'Hello';
   })
-  .controller('BarCtrl', function($scope){
-    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    $scope.series = ['Series A'];
-    $scope.data = [[65, 59, 80, 81, 56, 55, 40]];
+  .controller('PieCtrl', function($scope){
+    
+    $scope.poll = {
+      topic: "What is your favorite ocean?",
+      labels: ['Pacific', 'Atlantic', 'Indian'],
+      data: [81, 56, 55]
+      
+    };
+    $scope.options = ['Pacific','Atlantic'];
+    $scope.labels = ['Foo','Bar'];
+    $scope.data = [1,2];
+    $scope.topic = '';
     
     $scope.increaseData = function(){
-      $scope.data[0][0]++;
+      $scope.data[0]++;
     };
+    
+    $scope.addOption = function(){
+      $scope.options.push('Option' + ($scope.options.length + 1));
+    };
+    
+    $scope.addLabels = function(){
+      $scope.labels.push($scope.labels.length);
+      $scope.data.push(0);
+    };
+    
+    $scope.submitPoll = function(){
+      $scope.poll.data = $scope.data;
+      $scope.poll.labels = $scope.labels;
+      console.log($scope.data,$scope.labels);
+    }
   });
